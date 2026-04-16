e83c24a867e329af1217c75a068850a8b1152ef5
wadhah01 <agoubiwadhah@gmail.com>
2026-04-16 14:21:35 +0200

    correction

--- app/nos-specialites/page.tsx
+++ app/nos-specialites/page.tsx
@@ -3,12 +3,12 @@ import { SpecialtiesGrid } from '@/components/specialties/SpecialtiesGrid';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Nos Spécialités en Droit | Cabinet Dubois - Avocat Gabes,Tunisie',
-  description: 'Découvrez nos domaines d\'expertise : droit civil, commercial, immobilier, famille et succession. Accompagnement juridique professionnel à Paris.',
-  keywords: 'spécialités juridiques, droit civil, droit commercial, droit immobilier, droit famille, succession, avocat spécialisé Paris',
+  title: 'Nos Spécialités en Droit | Cabinet Issam Agoubi - Avocat Gabes,Tunisie',
+  description: 'Découvrez nos domaines d\'expertise : droit pénal, civil, commercial, immobilier, famille et succession. Accompagnement juridique professionnel à Gabès.',
+  keywords: 'spécialités juridiques, droit civil, droit commercial, droit immobilier, droit famille, succession, avocat spécialisé Gabès',
   openGraph: {
     title: 'Nos Spécialités en Droit | Cabinet Agoubi Issam',
-    description: 'Domaines d\'expertise juridique : droit civil, commercial, immobilier',
+    description: 'Domaines d\'expertise juridique : droit pénal, civil, commercial et immobilier',
     url: 'https://cabinet-Issam-agoubi.tn/nos-specialites',
   },
 };
